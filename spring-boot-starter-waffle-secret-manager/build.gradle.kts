26a17e6e7ed9f91006d59c3f40bd3af204de0ae8
Hank-Choi <zlzlqlzl1@naver.com>
2025-03-25 17:19:20 +0900

    Clean up unused dependencies

--- spring-boot-starter-waffle-secret-manager/build.gradle.kts
+++ spring-boot-starter-waffle-secret-manager/build.gradle.kts
@@ -2,8 +2,8 @@ group = "com.wafflestudio.spring"
 dependencies {
     compileOnly("org.springframework.boot:spring-boot-starter-web")
 
-    implementation("software.amazon.awssdk:secretsmanager:2.20.66")
-    implementation("software.amazon.awssdk:sts:2.20.66")
+    compileOnly("software.amazon.awssdk:secretsmanager:2.20.66")
+    compileOnly("software.amazon.awssdk:sts:2.20.66")
 
     testImplementation(kotlin("test"))
 }
--- spring-boot-starter-waffle/build.gradle.kts
+++ spring-boot-starter-waffle/build.gradle.kts
@@ -1,5 +1,4 @@
 dependencies {
     implementation(project(":truffle:spring-boot-starter-truffle"))
-    implementation(project(":truffle:truffle-logback"))
     implementation(project(":spring-boot-starter-waffle-secret-manager"))
 }
