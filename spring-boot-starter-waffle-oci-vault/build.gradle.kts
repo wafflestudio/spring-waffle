586e413e06c45afdfe1813b0369aa2014cc52274
Chanyeong Lim <asp345@posteo.net>
2026-02-14 14:58:38 +0900

    do not exclude hk2

--- spring-boot-starter-waffle-oci-vault/build.gradle.kts
+++ spring-boot-starter-waffle-oci-vault/build.gradle.kts
@@ -5,10 +5,7 @@ dependencies {
 
     // OCI Vault (Secrets) access via OCI Java SDK.
     implementation("com.oracle.oci.sdk:oci-java-sdk-secrets:3.80.1")
-    implementation("com.oracle.oci.sdk:oci-java-sdk-common-httpclient-jersey3:3.80.1") {
-        // Avoid pulling HK2 injection implementation transitively.
-        exclude(group = "org.glassfish.jersey.inject", module = "jersey-hk2")
-    }
+    implementation("com.oracle.oci.sdk:oci-java-sdk-common-httpclient-jersey3:3.80.1")
 
     testImplementation(kotlin("test"))
 }
